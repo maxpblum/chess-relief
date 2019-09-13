7d453750b7d1de00e907a2e79913aef4674df1d2
Max Blum-Campo <max.blum@gmail.com>
2019-09-13 09:25:43 -0700

    Add basic bishop tests

---
+++ tests/TestBishop.ml
@@ -0,0 +1,49 @@
+open OUnit2;;
+open TestUtil;;
+open Color;;
+
+let test_basic_motion _ =
+    assert_legal_move
+        ~msg:"Can move forward-left"
+        [(3,3,White,Bishop)] White (3,3) (6,0) [(6,0,White,Bishop)]
+    ;
+
+    assert_legal_move
+        ~msg:"Can move forward-right"
+        [(3,3,White,Bishop)] White (3,3) (6,6) [(6,6,White,Bishop)]
+    ;
+
+    assert_legal_move
+        ~msg:"Can move backward-left"
+        [(3,3,White,Bishop)] White (3,3) (0,0) [(0,0,White,Bishop)]
+    ;
+
+    assert_legal_move
+        ~msg:"Can move backward-right"
+        [(3,3,White,Bishop)] White (3,3) (0,6) [(0,6,White,Bishop)]
+    ;
+
+    assert_illegal_move
+        ~msg:"Cannot move laterally"
+        [(3,3,White,Bishop)] White (3,3) (3,0)
+    ;
+
+    assert_illegal_move
+        ~msg:"Cannot move vertically"
+        [(3,3,White,Bishop)] White (3,3) (6,3)
+    ;
+
+    assert_legal_move
+        ~msg:"Can capture"
+        [(3,3,White,Bishop);(6,6,Black,Pawn)] White (3,3) (6,6) [(6,6,White,Bishop)]
+    ;
+
+    assert_illegal_move
+        ~msg:"Cannot move through a piece"
+        [(3,3,White,Bishop);(6,6,Black,Pawn);(7,7,Black,Knight)] White (3,3) (7,7)
+
+let suite =
+"suite">:::
+ [
+     "test basic motion">:: test_basic_motion;
+ ]
