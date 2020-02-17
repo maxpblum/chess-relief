d03d6f891d1a74594c41a1158464ba552cb6e395
Max Blum-Campo <max.blum@gmail.com>
2020-02-17 09:43:50 -0500

    Implement castling including some refactoring/reorganizing

--- src/GameState.ml
+++ src/GameState.ml
@@ -13,6 +13,107 @@ type attempted_move_t =
     | Illegal of IllegalMoveReason.t
     | Legal   of t
 
+let initial = {board=Board.initial;turn=Color.White}
+
+let king_location (color : Color.t) board =
+    let open Rank in
+    let is_king Board.{piece={rank}} = match rank with
+    | King _ -> true
+    | _ -> false
+    in
+    let open Board in
+    match (all_pieces_of_color color board |> List.filter is_king) with
+    (* This shouldn't happen *)
+    | [] -> None
+    | {location} :: _ -> Some location
+
+let delta_matches move potential_move =
+    let open Board in
+    let open PotentialMove in
+    (* To match pawn exchange moves, pretend they are not pawn exchange moves,
+     * since move_of_delta always outputs a non-pawn exchange. *)
+    ({move with replacement=None} = move_of_delta potential_move.delta move.from)
+
+let mark_rook_or_king_moved location board =
+    let open Board in
+    let open Rank in
+    match get_value_at location board with
+    | None -> board
+    | Some piece -> match piece with
+    | {color;rank=King false} ->
+            set_location location (Some {color;rank=King true}) board
+    | {color;rank=Rook false} ->
+            set_location location (Some {color;rank=Rook true}) board
+    | _ -> board
+
+let realize_potential_move move board =
+    let open Board in
+    let open Rank in
+    let king_and_rook_marked_board = mark_rook_or_king_moved move.from board in
+    let mark_false_if_pawn board (space:space_t) location = match space with
+    | Some {rank=Pawn true;color} -> set_location location (Some {rank=Pawn false;color}) board
+    | _ -> board
+    in
+    (* Use the board fold function to iterate through the spaces on the existing
+     * board, building up a newly marked version of the board. We pass in the same
+     * board argument twice since fold doesn't know we're using the board as our
+     * accumulator. *)
+    let pawn_marked_board =
+            Board.fold
+                mark_false_if_pawn
+                king_and_rook_marked_board
+                king_and_rook_marked_board
+    in
+    let open PotentialMove in
+    function
+    | NormalMove -> make_move move pawn_marked_board
+    | EnPassant -> (
+        let captured_pawn_row =
+            (* Capturing a black pawn that just jumped to row four. *)
+            if move.destination.row >= 4 then 4
+            (* Capturing a white pawn that just jumped to row three. *)
+            else 3
+        in
+        let captured_pawn_spot = {
+            row=captured_pawn_row ;
+            col=move.destination.col ;
+        } in
+        pawn_marked_board |>
+        make_move move |>
+        set_location captured_pawn_spot None
+    )
+    | PawnJump -> (
+        match (pawn_marked_board |> get_value_at move.from) with
+        (* Should not be possible *)
+        | None -> Board.initial
+        | Some {color} ->
+            make_move move pawn_marked_board |>
+            set_location move.destination (Some {color;rank=Pawn true})
+    )
+    | Castling -> (
+        let row = move.from.row in
+        let rook_from_column = if move.destination.col < 3 then 0 else 7 in
+        let rook_to_column = if rook_from_column < 3 then 3 else 5 in
+        let rook_origin = {row;col=rook_from_column} in
+        let rook_destination = {row;col=rook_to_column} in
+        pawn_marked_board |>
+        make_move move |>
+        make_move {null_move with from=rook_origin ; destination=rook_destination} |>
+        mark_rook_or_king_moved rook_destination
+    )
+    | PawnExchange -> (
+        match (pawn_marked_board |> get_value_at move.from) with
+        (* Should not be possible *)
+        | None -> Board.initial
+        | Some {color} ->
+        match move.replacement with
+        (* Should not be possible *)
+        | None -> Board.initial
+        | Some rank ->
+            make_move move pawn_marked_board |>
+            set_location move.destination (Some {color;rank})
+    )
+
 let rec failed_condition move board cond =
     let open Rank in
     let open Board in
@@ -74,78 +175,24 @@ let rec failed_condition move board cond =
     | MovingToEndRow -> wrap_bool (destination.row == 0 || destination.row == 7)
     | MoveSpecifiesReplacement ->
             wrap_bool (match move.replacement with None -> false | Some _ -> true)
-    | SpaceNotThreatened delta -> wrap_bool true
-    | PieceHasNotMoved delta -> wrap_bool true
-
-let realize_potential_move move board =
-    let open Board in
-    let open Rank in
-    let king_and_rook_marked_board = (
-        match get_value_at move.from board with
-        | None -> board
-        | Some piece -> match piece with
-        | {color;rank=King false} ->
-                set_location move.from (Some {color;rank=King true}) board
-        | {color;rank=Rook false} ->
-                set_location move.from (Some {color;rank=Rook true}) board
-        | _ -> board
-    )
-    in
-    let mark_false_if_pawn board (space:space_t) location = match space with
-    | Some {rank=Pawn true;color} -> set_location location (Some {rank=Pawn false;color}) board
-    | _ -> board
-    in
-    (* Use the board fold function to iterate through the spaces on the existing
-     * board, building up a newly marked version of the board. We pass in the same
-     * board argument twice since fold doesn't know we're using the board as our
-     * accumulator. *)
-    let pawn_marked_board =
-            Board.fold
-                mark_false_if_pawn
-                king_and_rook_marked_board
-                king_and_rook_marked_board
-    in
-    let open PotentialMove in
-    function
-    | NormalMove -> make_move move pawn_marked_board
-    | EnPassant -> (
-        let captured_pawn_row =
-            (* Capturing a black pawn that just jumped to row four. *)
-            if move.destination.row >= 4 then 4
-            (* Capturing a white pawn that just jumped to row three. *)
-            else 3
-        in
-        let captured_pawn_spot = {
-            row=captured_pawn_row ;
-            col=move.destination.col ;
-        } in
-        pawn_marked_board |>
-        make_move move |>
-        set_location captured_pawn_spot None
-    )
-    | PawnJump -> (
-        match (pawn_marked_board |> get_value_at move.from) with
-        (* Should not be possible *)
-        | None -> Board.initial
+    | SpaceNotThreatened delta -> wrap_bool (
+        match get_value_at from board with
+        (* Should not happen *)
+        | None -> false
         | Some {color} ->
-            make_move move pawn_marked_board |>
-            set_location move.destination (Some {color;rank=Pawn true})
+        let {destination=space} = move_of_delta delta from in
+        not (is_threatened_by (Color.opposite color) board space)
     )
-    | Castling -> Board.initial
-    | PawnExchange -> (
-        match (pawn_marked_board |> get_value_at move.from) with
-        (* Should not be possible *)
-        | None -> Board.initial
-        | Some {color} ->
-        match move.replacement with
-        (* Should not be possible *)
-        | None -> Board.initial
-        | Some rank ->
-            make_move move pawn_marked_board |>
-            set_location move.destination (Some {color;rank})
+    | PieceHasNotMoved delta -> wrap_bool (
+        let {destination=space} = move_of_delta delta from in (
+            match get_value_at space board with
+            | Some {rank=Rook false} -> true
+            | Some {rank=King false} -> true
+            | _ -> false
+        )
     )
 
-let attempt_potential_move board from replacement potential_move =
+and attempt_potential_move board from replacement potential_move =
     let open PotentialMove in
     let {condition;special_move_type;delta} = potential_move in
     let open Board in
@@ -155,21 +202,7 @@ let attempt_potential_move board from replacement potential_move =
     | Some cond -> NonThreat (FailedCondition cond)
     | None -> Threat (realize_potential_move move board special_move_type)
 
-let initial = {board=Board.initial;turn=Color.White}
-
-let king_location (color : Color.t) board =
-    let open Rank in
-    let is_king Board.{piece={rank}} = match rank with
-    | King _ -> true
-    | _ -> false
-    in
-    let open Board in
-    match (all_pieces_of_color color board |> List.filter is_king) with
-    (* This shouldn't happen *)
-    | [] -> None
-    | {location} :: _ -> Some location
-
-let rec try_potential_moves board from replacement last_illegal_reason
+and try_potential_moves board from replacement last_illegal_reason
   : PotentialMove.t list -> possible_threat_t
   = function
     | [] -> NonThreat last_illegal_reason
@@ -177,14 +210,7 @@ let rec try_potential_moves board from replacement last_illegal_reason
         | NonThreat i -> try_potential_moves board from replacement i pms
         | threat -> threat
 
-let delta_matches move potential_move =
-    let open Board in
-    let open PotentialMove in
-    (* To match pawn exchange moves, pretend they are not pawn exchange moves,
-     * since move_of_delta always outputs a non-pawn exchange. *)
-    ({move with replacement=None} = move_of_delta potential_move.delta move.from)
-
-let find_matching_threat board piece move =
+and find_matching_threat board piece move =
     let open IllegalMoveReason in
     let open Board in
     piece |>
@@ -192,7 +218,7 @@ let find_matching_threat board piece move =
     List.filter (delta_matches move) |>
     try_potential_moves board move.from move.replacement IllegalForPiece
 
-let piece_threatens_spot board spot Board.{piece;location} =
+and piece_threatens_spot board spot Board.{piece;location} =
     let open Board in
     let open Rank in
     (* Ensure pawn exchanges are recognized as valid. *)
@@ -201,7 +227,7 @@ let piece_threatens_spot board spot Board.{piece;location} =
     | NonThreat _ -> false
     | Threat _ -> true
 
-let is_threatened_by color board spot =
+and is_threatened_by color board spot =
     Board.all_pieces_of_color color board |>
     List.find_opt (piece_threatens_spot board spot) |>
     function
