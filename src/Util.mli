80bdb2ae81d74b7ab926b8403eba6120b70a073b
Max Blum-Campo <max.blum@gmail.com>
2020-02-17 14:57:53 -0500

    Remove remaining unused utilities

--- src/Util.mli
+++
@@ -1,3 +0,0 @@
-val iterate_array_backwards_impl : (int -> 'a -> 'b) -> 'a array -> int -> unit
-val iterate_array_backwards_i : (int -> 'a -> 'b) -> 'a array -> unit
-val iterate_array_backwards : ('a -> unit) -> 'a array -> unit
