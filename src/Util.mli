84792f7d9704fffc329395791599e410e404bad6
Max Blum-Campo <max.blum@gmail.com>
2020-02-17 10:12:04 -0500

    Add some iterable junk

--- src/Util.mli
+++ src/Util.mli
@@ -1,3 +1,14 @@
 val iterate_array_backwards_impl : (int -> 'a -> 'b) -> 'a array -> int -> unit
 val iterate_array_backwards_i : (int -> 'a -> 'b) -> 'a array -> unit
 val iterate_array_backwards : ('a -> unit) -> 'a array -> unit
+
+type ('a,'b) iterable_state_t = {
+    current: 'a ;
+    state: 'b ;
+}
+
+type ('a,'b) iterable_t =
+    | Done
+    | Ongoing of ('a,'b) iterable_state_t
+
+type ('a,'b) iterate_func_t = 'b -> ('a,'b) iterable_t
